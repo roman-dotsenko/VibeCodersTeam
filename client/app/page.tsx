fa35f02fd72fcd468846cb21dfc41590ba0c2a7f
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-11-07 16:01:25 +0200

    Commit

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "@/node_modules/react-i18next";
 import {useTranslations} from 'next-intl';
 import LanguageSwitcher from "../components/Language/LanguageSwitcher";
 import Header from "../components/Header/Header";
